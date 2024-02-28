53dfef10af4403cb484d6049b2b1fb7d34e3ab7a
izourne <izourne003@gmail.com>
2024-02-28 21:07:25 +0100

    specify only one access origin

--- server/app.js
+++ server/app.js
@@ -47,7 +47,10 @@ app.use(multer({ storage: fileStorage, fileFilter }).single('image'))
 // set headers
 app.use((req, res, next) => {
   // cors
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://mern-sociomedia-react.vercel.app'
+  )
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE, OPTIONS'
